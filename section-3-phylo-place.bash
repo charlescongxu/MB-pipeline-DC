174a85be610e800b3380deed1159744d5dbc1aa4
Charles Xu <charles.cong.xu@gmail.com>
2016-03-24 00:33:32 +0800

    Update section-3-phylo-place.bash

--- section-3-phylo-place.bash
+++ section-3-phylo-place.bash
@@ -42,7 +42,7 @@ perl format_conversion.pl aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq 
 
 # Note: may need to remove some sequences consisting entirely of undetermined values (-) or else RAxML will not run, these are not the taxa you are looking for...
 
-sed '/_1199\|_44259\|_23857\|_24008\|_63886/{N;d;}' aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy > fixed.aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy
+sed '/_32350\|_35659/{N;d;}' aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy > fixed.aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy
 
 /home/wangxy/scripts/RAxML-7.2.8-ALPHA/raxmlHPC -f v -t RAxML_bestTree.ref_tree -m GTRCAT -n BWL_CROP98.cluster.align.raxmlEPAout -s fixed.aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy -o Tachyglossus_aculeatus
 
@@ -71,7 +71,7 @@ cd ../pplacer
 cp ../../query/BWL_CROP98.cluster.align .
 cp ../../query/refs_unaligned.prank.best.fas.overlapping .
 cp ../../references/RAxML_bestTree.ref_tree .
-cp ../../reference/RAxML_info.ref_tree .
+cp ../../references/RAxML_info.ref_tree .
 
 # concatenate aligned queries with 'overlapping' reference alignment and then convert from fasta to phylip format
 
@@ -83,7 +83,7 @@ pplacer-Linux-v1.1.alpha17/pplacer --keep-at-most 1 -t RAxML_bestTree.ref_tree -
 
 # Note: may need to remove some sequences consisting entirely of undetermined values (-) or else RAxML will not run, these are not the taxa you are looking for...
 
-sed '/_1199\|_44259\|_23857\|_24008\|_63886/{N;d;}' aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.fa > fixed.aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.fa
+sed '/_32350\|_35659/{N;d;}' aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.fa > fixed.aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.fa
 
 pplacer-Linux-v1.1.alpha17/pplacer --keep-at-most 1 -t RAxML_bestTree.ref_tree -s RAxML_info.ref_tree fixed.aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.fa
 
@@ -113,14 +113,30 @@ bagpipe_phylo.pl
 (https://sourceforge.net/projects/bagpipe/)
 
 # input files:
+refs_unaligned.prank.best.fas.overlapping = aligned references that overlap with the supertree backbone
+BE_mammal_supertree.nwk.overlapping       = overlapping supertree backbone (from section 1 when making the reference database)
+BWL_CROP98.cluster.align                  = alignment of queries to reference, output from mothur
 
-aligned queries and references that overlap with supertree backbone in phylip format (from RAxML EPA placement option)
-overlapping supertree backbone (from section 1 when making the reference database)
-
-# make a new directory (bagpipe_phylo) for placement option 3 and copy input files into it
+# make a new directory (bagpipe_phylo) for placement option 3 and copy input files and 'format_conversion.pl' into it
 
 mkdir ../bagpipe_phylo
 cd ../bagpipe_phylo
+
+cp ../../query/refs_unaligned.prank.best.fas.overlapping .
+cp ../../query/BWL_CROP98.cluster.align .
+cp ../../references/BE_mammal_supertree.nwk.overlapping .
+cp ../../references/format_conversion.pl .
+
+# we need an alignment of both the references and the queries
+# we use the 'overlapping' file because it contains accessions rm, which matches the constraint tree
+
+# concatenate aligned queries with 'overlapping' reference alignment and then convert from fasta to phylip format
+
+cat refs_unaligned.prank.best.fas.overlapping BWL_CROP98.cluster.align > aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq
+perl format_conversion.pl aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy fasta phylip
+
+# output files:
+#  aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy
 
 # use RAxML to create a constraint tree from the input files
 
@@ -133,14 +149,23 @@ cd ../bagpipe_phylo
 #  RAxML_log.BWL_CROP98.constrn
 #  RAxML_result.BWL_CROP98.constrn
 
+# Note: may need to remove some sequences consisting entirely of undetermined values (-) or else RAxML will not run, these are not the taxa you are looking for...
+
+sed '/_32350\|_35659/{N;d;}' aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy > fixed.aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy
+
+/home/wangxy/scripts/RAxML-7.2.8-ALPHA/raxmlHPC -s fixed.aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.phy -n BWL_CROP98.constrn -m GTRCAT -c 25 -p 12345 -g BE_mammal_supertree.nwk.overlapping
+
+# Also note: make sure the header of your queries/references alignment is correct, the 1st number should be the number of total sequences
+# otherwise you will get this stupid error: "Taxon Name too long at taxon XXX, adapt constant nmlngth in axml.h, current setting 256"
+# the number of sequences is equal to 1 less than the number of lines, which you can check using 'wc <file>'
+
+# output files:
+#  
+
 # copy nodes.dmp and names.dmp from section 1 when making the reference database to bagpipe directory
 
 cp ../../references/nodes.dmp .
 cp ../../references/names.dmp .
-
-# copy aligned queries (output from mothur) to bagpipe directory
-
-cp ../../query/BWL_CROP98.cluster.align .
 
 # use perl script 'bagpipe_phylo.pl' to do phylogenetic placement using the constrained RAxML tree and the aligned queries
 
@@ -148,7 +173,7 @@ perl bagpipe_phylo.pl -treefile RAxML_bestTree.BWL_CROP98.constrn -seqfile BWL_C
 
 # Note: may need to remove some sequences consisting entirely of undetermined values (-) or else RAxML will not run, these are not the taxa you are looking for...
 
-sed '/_1199\|_44259\|_23857\|_24008\|_63886/{N;d;}' BWL_CROP98.cluster.align > fixed.BWL_CROP98.cluster.align
+sed '/_32350\|_35659/{N;d;}' BWL_CROP98.cluster.align > fixed.BWL_CROP98.cluster.align
 
 perl bagpipe_phylo.pl -treefile RAxML_bestTree.BWL_CROP98.constrn -seqfile fixed.BWL_CROP98.cluster.align -support 0 -node 40674
 
