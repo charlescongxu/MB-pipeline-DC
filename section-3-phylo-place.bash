96c20661420aa76565db35c1b4d42eb7479df4aa
Charles Xu <charles.cong.xu@gmail.com>
2016-07-11 12:38:34 +0800

    Update section-3-phylo-place.bash

--- section-3-phylo-place.bash
+++ section-3-phylo-place.bash
@@ -116,7 +116,7 @@ cp ../../references/RAxML_info.ref_tree .
 
 # use pplacer with reference tree and concatenated aligned queries with 'overlapping' reference alignment to do phylogenetic placement
 
-/home/wangxy/scripts/pplacer/pplacer-Linux-v1.1.alpha17/pplacer --keep-at-most 7 -t RAxML_bestTree.ref_tree_pplacer -s RAxML_info.ref_tree aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.fa
+/home/wangxy/scripts/pplacer/pplacer-Linux-v1.1.alpha17/pplacer --keep-at-most 7 -t RAxML_bestTree.ref_tree_pplacer -s RAxML_info.ref_tree_pplacer aligned_refs.BWL_CROP98.cluster.align.overlapping.rpq.fa
 
 # Note: may need to remove some sequences consisting entirely of undetermined values (-) or else RAxML will not run, these are not the taxa you are looking for...
 
